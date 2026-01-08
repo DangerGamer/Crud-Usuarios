26571403412254ac0c979435daa3e5bee6ed19c2
Darkus Magno <deyvidsolis16@gmail.com.com>
2026-01-07 21:20:08 -0500

    implementacion de pruebas de viemodels

--- app/src/test/java/com/dm/crudusuarios/ui/viewmodel/UsuarioViewModelTest.kt
+++ app/src/test/java/com/dm/crudusuarios/ui/viewmodel/UsuarioViewModelTest.kt
@@ -13,6 +13,8 @@ import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -78,5 +80,168 @@ class UsuarioViewModelTest{
 
         //then
         assertEquals(usuarios, viewModel.users.getOrAwaitValue())
+    }
+
+    @Test
+    fun `obtener usuarios respuesta din datos`() = runTest {
+        //Given
+        val usuarios = emptyList<UsuarioModel>()
+
+        val response = UsuarioResponse(
+            success = true,
+            data = usuarios,
+            message = "No hay datos"
+        )
+
+        coEvery { getUsersUseCase() } returns response
+
+        //When
+        viewModel.fetchUsers()
+
+        //Then
+        assertTrue(response.success)
+        assertEquals("No hay datos", response.message)
+    }
+
+    @Test
+    fun `obtener usuarios respuesta error desconocido` () = runTest {
+        //Given
+        val usuarios = emptyList<UsuarioModel>()
+
+        val response = UsuarioResponse(
+            success = false,
+            data = usuarios,
+            error = "Error desconocido"
+        )
+
+        coEvery { getUsersUseCase() } returns response
+
+        //When
+        viewModel.fetchUsers()
+
+        //Then
+        assertFalse(response.success)
+        assertEquals("Error desconocido", response.error)
+    }
+
+    @Test
+    fun `obtener usuarios retorna exception` () = runTest {
+        //Given
+        coEvery { getUsersUseCase() } throws RuntimeException("Network failure")
+
+        //When
+        viewModel.fetchUsers()
+
+        //Then
+        assertEquals("Error: Network failure", viewModel.error.value)
+    }
+
+    @Test
+    fun `envio de listado para eliminar usuarios de forma exitosa` () = runTest {
+        //Given
+        val usuarios = listOf(
+            UsuarioModel(
+                1,
+                "juan",
+                "solis",
+                "parra",
+                "calle 5",
+                "3137238908",
+                "j@gmal.com",
+                "M"
+            ),
+            UsuarioModel(
+                1,
+                "juan",
+                "solis",
+                "parra",
+                "calle 5",
+                "3137238908",
+                "j@gmal.com",
+                "M"
+            )
+        )
+        val fakeResponse = UsuarioResponse<Unit>(success = true, data = null, error = null, message = null)
+        coEvery { deleteUsersUseCase(any()) } returns fakeResponse
+
+        //When
+        viewModel.deleteUsers(usuarios)
+
+        //Then
+        assertTrue(viewModel.deleted.getOrAwaitValue())
+    }
+
+    @Test
+    fun `envio de listado de usuarios para eliminar retorna error` () = runTest {
+        //Given
+        val usuarios = listOf(
+            UsuarioModel(
+                1,
+                "juan",
+                "solis",
+                "parra",
+                "calle 5",
+                "3137238908",
+                "j@gmal.com",
+                "M"
+            ),
+            UsuarioModel(
+                1,
+                "juan",
+                "solis",
+                "parra",
+                "calle 5",
+                "3137238908",
+                "j@gmal.com",
+                "M"
+            )
+        )
+        val fakeResponse = UsuarioResponse<Unit>(
+            success = false,
+            data = null,
+            error = "Error desconocido"
+        )
+        coEvery { deleteUsersUseCase(any()) } returns fakeResponse
+
+        //When
+        viewModel.deleteUsers(usuarios)
+
+        //Then
+        assertFalse(fakeResponse.success)
+        assertEquals("Error desconocido", viewModel.error.value)
+    }
+
+    @Test
+    fun `envio de listado de usuarios para eliminar exception` () = runTest {
+        //Given
+        val usuarios = listOf(
+            UsuarioModel(
+                1,
+                "juan",
+                "solis",
+                "parra",
+                "calle 5",
+                "3137238908",
+                "j@gmal.com",
+                "M"
+            ),
+            UsuarioModel(
+                1,
+                "juan",
+                "solis",
+                "parra",
+                "calle 5",
+                "3137238908",
+                "j@gmal.com",
+                "M"
+            )
+        )
+        coEvery { deleteUsersUseCase(any()) } throws RuntimeException("Network failure")
+
+        //When
+        viewModel.deleteUsers(usuarios)
+
+        //Then
+        assertEquals("Error: Network failure", viewModel.error.value)
     }
 }
